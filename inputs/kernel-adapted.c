538f7be1801e3f7f2a18a5e8c9d9e2faf60f59f1
Marcos Horro <marcos.horro@udc.es>
2019-11-07 22:57:21 -0700

    Start working on it...

---
+++ inputs/kernel-adapted.c
@@ -0,0 +1,16 @@
+/**
+ * File              : kernel-adapted.c
+ * Author            : Marcos Horro <marcos.horro@udc.gal>
+ * Date              : Xov 07 Nov 2019 15:30:00 MST
+ * Last Modified Date: Xov 07 Nov 2019 15:45:03 MST
+ * Last Modified By  : Marcos Horro <marcos.horro@udc.gal>
+ */
+void kernel_spmvstyle(int m, int n, double A[m][n], double x[n], double y[n],
+                      int uI, int uIt, int uIs, int uJ, int uJt, int uJs) {
+  int i, j;
+
+  for (i = uI; i < uI + uIt; i += uIs)
+    for (j = uJ; j < uJ + uJt; j += uJs) {
+      y[i] = y[i] + A[i][j] * x[j];
+    }
+}
