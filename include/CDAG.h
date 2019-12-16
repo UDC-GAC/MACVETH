1fb4f54435e09fa456b1d51ac05d45f06ea38b77
Marcos Horro <marcos.horro@udc.es>
2019-12-16 16:23:00 -0700

    Still working on CDAG

--- include/CDAG.h
+++ include/CDAG.h
@@ -2,7 +2,7 @@
  * File              : CDAG.h
  * Author            : Marcos Horro <marcos.horro@udc.gal>
  * Date              : Lun 09 Dec 2019 15:10:51 MST
- * Last Modified Date: Xov 12 Dec 2019 15:13:27 MST
+ * Last Modified Date: Lun 16 Dec 2019 15:18:21 MST
  * Last Modified By  : Marcos Horro <marcos.horro@udc.gal>
  */
 #ifndef MACVETH_CDAG_H
@@ -25,41 +25,60 @@ inline static std::map<BinaryOperator::Opcode, std::string> BOtoValue = {
     {BO_Add, "add"},
     {BO_Sub, "sub"},
     {BO_Assign, "store"}};
+/// FIXME
+inline static std::map<std::string, std::list<int>> CostsMap = {
+    {"reduction", {1, 1, 1}}, {"load", {1, 1, 1}}, {"sub", {1, 1, 1}},
+    {"add", {1, 1, 1}},       {"div", {1, 1, 1}},  {"mul", {1, 1, 1}}};
 
 /// All Nodes belong to a CDAG. Each node or vertex holds information regarding
 /// the type of operation as well as its Edges (or links to another Nodes).
 /// Nodes also hold information regarding the scheduling.
 class Node {
 public:
+  /// Definition of NodeListType
+  typedef std::list<Node *> NodeListType;
+  /// Available types of nodes
+  enum NodeType { NODE_MEM, NODE_OP, UNDEF };
   /// Holds information regarding the scheduling for this Node
   struct SchedInfo {
-    int Plcmnt;
-    int Sched;
+    int FreeSched = 0;
+    int Plcmnt = 0;
+    int Sched = 0;
+  };
+  /// If the node is NODE_OP type, then it will hold a result value
+  struct OutputInfo {
+    std::string OutName = "";
   };
 
-  /// Definition of NodeListType
-  typedef std::list<Node> NodeListType;
-  /// Available types of nodes
-  enum NodeType { NODE_EXPR, NODE_OP, UNDEF };
   /// FIXME
-  inline static std::map<std::string, std::list<int>> CostsMap = {
-      {"reduction", {1, 1, 1}}, {"load", {1, 1, 1}}, {"sub", {1, 1, 1}},
-      {"add", {1, 1, 1}},       {"div", {1, 1, 1}},  {"mul", {1, 1, 1}}};
-
   /// This is the unique constructor for nodes, as we will creating Nodes from
   /// CDAG, so each TAC corresponds to a = b op c
   Node(TAC T) {
     this->T = NODE_OP;
     this->Value = BOtoValue[T.getOP()];
-    connectOutput(new Node(T.getA()));
+    this->O.OutName = T.getA()->getExprStr();
     connectInput(new Node(T.getB()));
     connectInput(new Node(T.getC()));
   }
 
-  /// TODO
+  /// This is the unique constructor for nodes, as we will creating Nodes from
+  /// CDAG, so each TAC corresponds to a = b op c
+  Node(TAC T, NodeListType L) {
+    this->T = NODE_OP;
+    this->Value = BOtoValue[T.getOP()];
+    this->O.OutName = T.getA()->getExprStr();
+    Node *NB = findOutputNode(T.getB()->getExprStr(), L);
+    Node *NC = findOutputNode(T.getC()->getExprStr(), L);
+    connectInput(NB == NULL ? new Node(T.getB()) : NB);
+    connectInput(NC == NULL ? new Node(T.getC()) : NC);
+  }
+
   /// Schedule info is needed for the algorithms to perform permutations in
   /// nodes
-  void setSchedInfo();
+  void setFreeSchedInfo(int Value);
+
+  /// Check if Node N is already in node list L
+  Node *findOutputNode(std::string NodeName, NodeListType L);
 
   /// Connect a Node as input
   void connectInput(Node *N);
@@ -67,16 +86,18 @@ public:
   void connectOutput(Node *N);
 
   /// Get the number of inputs in this Node
-  int getOutputNum() { return this->Out.size(); }
-  /// Get the output Node
-  NodeListType getOuput() { return this->Out; }
+  int getOutputNum() { return this->OutNodes.size(); }
+  /// Get the output Nodes
+  NodeListType getOuput() { return this->OutNodes; }
+  /// Get the output information
+  OutputInfo getOuputInfo() { return this->O; }
   /// Get the number of inputs in this Node
-  int getInputNum() { return this->In.size(); }
+  int getInputNum() { return this->I.size(); }
   /// Get the list of node inputs in this Node
-  NodeListType getInputs() { return this->In; }
+  NodeListType getInputs() { return this->I; }
 
   /// Checks if output list is zero
-  bool hasOut();
+  bool hasOutNodes();
   /// Checks if input list is zero
   bool hasInputs();
   /// Checks if output list is not zero
@@ -86,19 +107,17 @@ public:
   /// Checks if given node is in the input list
   bool hasInNode(Node *N);
 
-  /// Merge node
-  void mergeIfFound(Node NIn);
-  int replaceInputWithNode(Node *NewIn);
-
   /// Returns the string value of the node
   std::string getValue() const { return this->Value; }
 
   /// Two nodes are equal if and only if they have the same value. Seems pretty
   /// straigthforward but it must be defined someway.
   bool operator==(const Node &N) { return !getValue().compare(N.getValue()); }
+  bool operator==(const Node *N) { return !getValue().compare(N->getValue()); }
   /// Two nodes are not equal if and only if they have the same value. Seems
   /// pretty straigthforward but it must be defined someway.
   bool operator!=(const Node &N) { return !operator==(N); }
+  bool operator!=(const Node *N) { return !operator==(N); }
 
 private:
   /// Holds information regarding the scheduling
@@ -108,36 +127,47 @@ private:
   /// Type of node
   NodeType T = UNDEF;
   /// List of inputs for this node
-  NodeListType In;
-  /// List of outputs for this node
-  NodeListType Out;
+  NodeListType I;
+  /// Info regarding the output register/node
+  OutputInfo O;
+  /// List of output Nodes for this node
+  NodeListType OutNodes;
 
   /// When creating a Node from a TempExpr, the connections will be created by
   /// the TAC which creates this Node
   Node(MVExpr *TE) {
-    this->T = NODE_EXPR;
+    this->T = NODE_MEM;
     this->Value = TE->getExprStr();
   }
 };
 
+/// The CDAG only holds a list of Nodes which are of type NODE_OP
 class CDAG {
 public:
   /// Given a list of TACs, create its correspondent CDAG
   static CDAG *createCDAGfromTAC(TacListType TL);
-
-  /// TODO
   /// Compute cost model
-  static int costModel(CDAG *C);
+  static int computeCostModel(CDAG *C);
+  static int computeCostModel(Node::NodeListType NL);
 
+  /// Computer the free schedule, basically the topology order
+  void computeFreeSchedule(CDAG *C);
+  void computeFreeSchedule(Node::NodeListType NL);
+
+  /// Generate intrinsics
+  static int generateIntrinsics(CDAG *C);
   /// Get the node of list registered for this CDAG
-  Node::NodeListType getNodeList() { return this->NL; }
+  Node::NodeListType getNodeListOps() { return this->NLOps; }
+  /// Get the node of list registered for this CDAG
+  Node::NodeListType getNodeListMem() { return this->NLMem; }
 
 private:
   /// Insert TAC instruction to the CDAG
-  Node insertTac(TAC T);
-  /// Connect nodes
-  bool connectNode(Node N);
-  Node::NodeListType NL;
+  Node *insertTac(TAC T, Node::NodeListType L);
+  /// List of OP nodes
+  Node::NodeListType NLOps;
+  /// List of memory nodes
+  Node::NodeListType NLMem;
 };
 
 } // namespace macveth
--- src/CDAG.cpp
+++ src/CDAG.cpp
@@ -2,7 +2,7 @@
  * File              : CDAG.cpp
  * Author            : Marcos Horro <marcos.horro@udc.gal>
  * Date              : Lun 09 Dec 2019 15:10:35 MST
- * Last Modified Date: Xov 12 Dec 2019 15:15:38 MST
+ * Last Modified Date: Lun 16 Dec 2019 16:21:12 MST
  * Last Modified By  : Marcos Horro <marcos.horro@udc.gal>
  */
 
@@ -12,29 +12,36 @@ namespace macveth {
 
 // ---------------------------------------------
 void Node::connectInput(Node *N) {
-  this->In.push_back(*N);
-  N->Out.push_back(*this);
+  this->I.push_back(N);
+  N->OutNodes.push_back(this);
 }
 
 // ---------------------------------------------
-void Node::connectOutput(Node *N) {
-  this->Out.push_back(*N);
-  N->In.push_back(*this);
+void Node::connectOutput(Node *N) {}
+
+// ---------------------------------------------
+bool Node::hasOutNodes() { return (this->OutNodes.size() == 0); }
+
+// ---------------------------------------------
+bool Node::hasInputs() { return !(this->I.size() == 0); }
+
+// ---------------------------------------------
+bool Node::isTerminal() { return !(this->hasOutNodes()); }
+
+// ---------------------------------------------
+Node *Node::findOutputNode(std::string NodeName, NodeListType L) {
+  for (Node *NL : L) {
+    if (NodeName.compare(NL->getOuputInfo().OutName)) {
+      return NL;
+    }
+  }
+  return NULL;
 }
-
-// ---------------------------------------------
-bool Node::hasOut() { return !(this->Out.size() == 0); }
-
-// ---------------------------------------------
-bool Node::hasInputs() { return !(this->In.size() == 0); }
-
-// ---------------------------------------------
-bool Node::isTerminal() { return !this->hasOut(); }
 
 // ---------------------------------------------
 bool Node::hasOutNode(Node *N) {
-  for (Node C : this->Out) {
-    if (&C == N) {
+  for (Node *C : this->OutNodes) {
+    if (C == N) {
       return true;
     }
   }
@@ -43,8 +50,8 @@ bool Node::hasOutNode(Node *N) {
 
 // ---------------------------------------------
 bool Node::hasInNode(Node *N) {
-  for (Node C : this->In) {
-    if (&C == N) {
+  for (Node *C : this->I) {
+    if (C == N) {
       return true;
     }
   }
@@ -52,37 +59,51 @@ bool Node::hasInNode(Node *N) {
 }
 
 // ---------------------------------------------
-void Node::mergeIfFound(Node NIn) {
-  if (!this->hasInputs()) {
-    return;
-  }
-  for (Node C : this->getInputs()) {
-    if (C == NIn) {
-      std::cout << "Merging node " + C.getValue() << "\n" << std::flush;
-      this->getInputs().remove(C);
-      this->getInputs().push_back(C);
-      return;
+void Node::setFreeSchedInfo(int Value) { this->SI.FreeSched = Value; }
+
+// ---------------------------------------------
+int computeMaxDepth(Node *N) {
+  /// This algorithm is not efficient if the CDAG is big... At some point more
+  /// intelligent strategies should be used instead
+  if (N->getInputNum() == 0) {
+    return 0;
+  } else {
+    int max = 0;
+    for (Node *T : N->getInputs()) {
+      max = std::max(max, computeMaxDepth(T));
     }
+    return 1 + max;
   }
 }
 
 // ---------------------------------------------
-Node CDAG::insertTac(TAC T) {
-  Node NewNode(T);
-  // this->NL.push_back(NewNode);
+int CDAG::computeCostModel(CDAG *G) {
+  int Class = 0;
+  int VL = 4;
+  std::string VLoad[VL];
+  std::string VOps[VL];
+  int Cursor = 0;
+  for (Node *N : G->getNodeListOps()) {
+  }
+}
+
+// ---------------------------------------------
+void CDAG::computeFreeSchedule(Node::NodeListType NL) {
+  for (Node *N : NL) {
+    N->setFreeSchedInfo(computeMaxDepth(N));
+  }
+}
+
+// ---------------------------------------------
+void CDAG::computeFreeSchedule(CDAG *C) {
+  CDAG::computeFreeSchedule(C->getNodeListOps());
+}
+
+// ---------------------------------------------
+Node *CDAG::insertTac(TAC T, Node::NodeListType L) {
+  Node *NewNode = new Node(T, L);
+  this->NLOps.push_back(NewNode);
   return NewNode;
-}
-
-// ---------------------------------------------
-bool CDAG::connectNode(Node NewNode) {
-  std::cout << "Connecting node " + NewNode.getValue() << "\n" << std::flush;
-  for (Node PN : this->getNodeList()) {
-    for (Node OutN : PN.getOuput()) {
-      NewNode.mergeIfFound(OutN);
-    }
-  }
-  this->NL.push_back(NewNode);
-  return true;
 }
 
 // ---------------------------------------------
@@ -95,8 +116,7 @@ CDAG *CDAG::createCDAGfromTAC(TacListType TL) {
     /// between the inputs and outputs. Actually we are looking for outputs of
     /// of already connected Nodes that match the input of this new NODE_OP.
     /// Looking for inputs
-    Node ActualNode = G->insertTac(T);
-    G->connectNode(ActualNode);
+    Node *ActualNode = G->insertTac(T, G->getNodeListOps());
   }
   return G;
 }
