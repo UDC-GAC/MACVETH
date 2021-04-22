6300a4cc98a79fde430ba95528a37f364ea71681
Markos Horro <marcos.horro@udc.es>
2021-04-22 18:08:07 +0200

    Updated license headers in source files. Added template as well

--- src/StmtWrapper.cpp
+++ src/StmtWrapper.cpp
@@ -1,30 +1,27 @@
-/**
- * File              : StmtWrapper.cpp
- * Author            : Marcos Horro <marcos.horro@udc.gal>
- * Date              : Lun 25 Nov 2019 13:48:24 MST
- * Last Modified Date: Lun 23 Mar 2020 18:29:59 CET
- * Last Modified By  : Marcos Horro <marcos.horro@udc.gal>
- *
- * Copyright (c) 2019-2020 Marcos Horro <marcos.horro@udc.gal>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+// MACVETH - StmtWrapper.cpp
+//
+// Copyright (c) Colorado State University. 2019-2021
+// Copyright (c) Universidade da Coruña. 2020-2021
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Authors:
+//     Marcos Horro <marcos.horro@udc.es>
+//     Louis-Nöel Pouchet <pouchet@colostate.edu>
+//     Gabriel Rodríguez <grodriguez@udc.es>
+//
+// Contact:
+//     Louis-Nöel Pouchet <pouchet@colostate.edu>
 
 #include "include/StmtWrapper.h"
 #include "include/MVAssert.h"
@@ -49,14 +46,14 @@ std::list<StmtWrapper *> StmtWrapper::genStmtWraps(CompoundStmt *CS,
     if (!ST) {
       continue;
     }
-    unsigned int Start =
+    unsigned Start =
         Utils::getSourceMgr()->getExpansionLineNumber(ST->getBeginLoc());
-    unsigned int End =
+    unsigned End =
         Utils::getSourceMgr()->getExpansionLineNumber(ST->getEndLoc());
     if ((Scop->StartLine <= Start) && (Scop->EndLine >= End)) {
       Utils::printDebug("StmtWrapper genStmtWraps",
                         "new StmtWrapper =\n" + Utils::getStringFromStmt(ST));
-      StmtWrapper *NewStmt = new StmtWrapper(ST);
+      auto NewStmt = new StmtWrapper(ST);
       SList.push_back(NewStmt);
     } else {
       // Could be inside the loop the region of interest
@@ -336,7 +333,6 @@ bool StmtWrapper::unrollAndJam(std::list<LoopInfo> LI, ScopLoc *Scop) {
 bool StmtWrapper::unrollByDim(std::list<LoopInfo> LI, ScopLoc *Scop) {
   bool FullUnroll = true;
   if (this->isLoop()) {
-    // Utils::printDebug("StmtWrapper", "unrollByDim loop");
     for (auto D : Scop->PA.UnrollDim) {
       if (this->LoopL.Dim == std::get<0>(D)) {
         if (Scop->PA.FullUnroll[this->LoopL.Dim]) {
